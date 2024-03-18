78c9ba9268c2117499f9e41c2dec89e9099e036a
Asurudo Jyo <asurudo@126.com>
2024-03-18 17:01:55 +0900

    完成 Lesson 3

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor blue = TGAColor(0, 0, 255, 255);
 
 Model *model = NULL;
 const int width = 800;
@@ -37,60 +38,85 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
 }
 
 // 计算点p的重心坐标
-Vec3f barycentric(Vec2i *pts, Vec2i P) {
-  Vec3f u = Vec3f(pts[2].x - pts[0].x, pts[1].x - pts[0].x, pts[0].x - P.x) ^
-            Vec3f(pts[2].y - pts[0].y, pts[1].y - pts[0].y, pts[0].y - P.y);
-  if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
-  return Vec3f(1.0 - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+  Vec3f s[2];
+  for (int i = 2; i--;) {
+    s[i][0] = C[i] - A[i];
+    s[i][1] = B[i] - A[i];
+    s[i][2] = A[i] - P[i];
+  }
+  Vec3f u = cross(s[0], s[1]);
+  if (std::abs(u[2]) > 1e-2)  // dont forget that u[2] is integer. If it is zero
+                              // then triangle ABC is degenerate
+    return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+  return Vec3f(-1, 1, 1);  // in this case generate negative coordinates, it
+                           // will be thrown away by the rasterizator
 }
 
 // 画一个三角形
-void triangle(Vec2i *pts, TGAImage &image, TGAColor color) {
-  Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
-  Vec2i bboxmax(0, 0);
-  Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+  Vec2f bboxmin(std::numeric_limits<float>::max(),
+                std::numeric_limits<float>::max());
+  Vec2f bboxmax(-std::numeric_limits<float>::max(),
+                -std::numeric_limits<float>::max());
+  Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
   for (int i = 0; i < 3; i++) {
-    bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
-    bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
-
-    bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
-    bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
-  }
-  Vec2i P;
-  for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
-    for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-      Vec3f bc_screen = barycentric(pts, P);
-      if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-      image.set(P.x, P.y, color);
+    for (int j = 0; j < 2; j++) {
+      bboxmin[j] = std::max(0.f, std::min(bboxmin[j], pts[i][j]));
+      bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
     }
   }
+  Vec3f P;
+  for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+    for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+      Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
+      if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+      P.z = 0;
+      for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];
+      if (zbuffer[int(P.x + P.y * width)] < P.z) {
+        zbuffer[int(P.x + P.y * width)] = P.z;
+        image.set(P.x, P.y, color);
+      }
+    }
+  }
+}
+
+// 模型坐标转换成世界坐标，也就是[-1, 1] 映射到 [0, width/height]
+Vec3f world2screen(Vec3f v) {
+  return Vec3f(int((v.x + 1.) * width / 2. + .5),
+               int((v.y + 1.) * height / 2. + .5), v.z);
 }
 
 int main() {
   model = new Model("obj/african_head.obj");
-  TGAImage image(width, height, TGAImage::RGB);
 
-  Vec3f light_dir(0, 0, -1);  // define light_dir
+  // 初始化zbuffer数组为负无穷
+  float *zbuffer = new float[width * height];
+  for (int i = width * height; i--;
+       zbuffer[i] = -std::numeric_limits<float>::max())
+    ;
+
+  TGAImage image(width, height, TGAImage::RGB);
+  Vec3f light_dir(0, 0, -1);
 
   for (int i = 0; i < model->nfaces(); i++) {
     std::vector<int> face = model->face(i);
-    Vec2i screen_coords[3];
+    Vec3f screen_coords[3];
     Vec3f world_coords[3];
     for (int j = 0; j < 3; j++) {
       Vec3f v = model->vert(face[j]);
-      screen_coords[j] =
-          Vec2i((v.x + 1.) * width / 2.0, (v.y + 1.0) * height / 2.0);
+      screen_coords[j] = world2screen(v);
       world_coords[j] = v;
     }
     // 计算法线
-    Vec3f n = (world_coords[2] - world_coords[0]) ^
-              (world_coords[1] - world_coords[0]);
+    Vec3f n = cross(world_coords[2] - world_coords[0],
+                    world_coords[1] - world_coords[0]);
     n.normalize();
     // 法线在光线方向的分量，垂直于光线方向的话就是1
     float intensity = n * light_dir;
     if (intensity > 0) {
       triangle(
-          screen_coords, image,
+          screen_coords, zbuffer, image,
           TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
     }
   }
